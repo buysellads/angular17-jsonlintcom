39f7f1ead3c34c4bf16200cc41305be8de0c8a4c
Sayz Lim <github@sayzlim.net>
2024-10-21 22:39:17 +0700

    Load Carbon Ads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { RouterLink, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 
+declare var _carbonads: any;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,10 +15,27 @@ export class AppComponent implements AfterViewInit {
 
   constructor(private router: Router) {}
 
+  loadCarbonScript(targetElement: HTMLElement) {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.carbonads.com/carbon.js?serve=CVAIKKQM&placement=carbonadsnet&format=responsive';
+    script.id = '_carbonads_js';
+    script.async = true;
+    targetElement.appendChild(script);
+  }
+
   ngAfterViewInit() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         console.log('Page routed to:', event.url);
+        const carbonContainer = document.querySelector("#carbonContainer") as HTMLElement;
+        const carbonAdElement = document.querySelector("#carbonads") as HTMLElement;
+        
+        if(carbonAdElement == null) {
+          this.loadCarbonScript(carbonContainer);
+        } else {
+          _carbonads.refresh();
+        }
+
       }
     });
   }
